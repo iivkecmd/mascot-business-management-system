ea73118d54e070f0f015c3164d245804f7ac8197
iivkecmd <samopjaniva@gmail.com>
2026-08-05 14:41:35 +0200

    Add endpoint for listing mascots

---
+++ MascotBusinessManagement/MascotBusiness.Api/Controllers/MascotsController.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using MascotBusiness.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MascotBusiness.Api.Controllers
+{
+   
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MascotsController: ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MascotsController(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMascots() {
+
+            var mascots = await _context.Mascots.ToListAsync();
+            return Ok(mascots);
+        }
+    }
+}
